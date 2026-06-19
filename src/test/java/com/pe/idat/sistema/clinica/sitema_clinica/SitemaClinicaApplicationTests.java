7b02a89a5836c1289294f7757e4df2a57b9dad91
Nxjose18 <59268915+Nxjose18@users.noreply.github.com>
2026-06-19 13:33:15 +0000

    primera act

---
+++ src/test/java/com/pe/idat/sistema/clinica/sitema_clinica/SitemaClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.idat.sistema.clinica.sitema_clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitemaClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
